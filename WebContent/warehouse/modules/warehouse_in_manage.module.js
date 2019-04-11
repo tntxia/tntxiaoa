487fdac6573b30e205941491864973ed19115728
tntxia <tntxia@163.com>
2019-04-11 21:09:00 +0800

    显示不了

--- WebContent/warehouse/modules/warehouse_in_manage.module.js
+++ WebContent/warehouse/modules/warehouse_in_manage.module.js
@@ -19,9 +19,8 @@ var warehousePath = webRoot+'/warehouse/warehouse.dispatch';
 	
 	tabs.addTab({
 		label:'待入库采购订单',
-		template:'template_wait.mvc',
+		template:'in/template_wait.mvc',
 		handler:function(){
-			
 			let target = $("#rkList");
 			let url = webRoot+'/purchasing/purchasing!listWaitRk.do';
 			
@@ -58,66 +57,20 @@ var warehousePath = webRoot+'/warehouse/warehouse.dispatch';
 			});
 			grid.init();
 			
-			$("#rkList").datagrid({
-				title:'待入库列表 ',
-				pageSize:20,
-				form:{
-					inputs:[{
-						label:"采购编号：",
-						name:"cgnumber"
-					},{
-						label:"供 应 商：",
-						name:"coname"
-					},{
-						label:"型号：",
-						name:"epro"
-					},{
-						label:'品牌',
-						name:'supplier'
-					}],
-					operations:[{
-						text:'查询',
-						click:function(){
-							var model = $("[name=epro]").val();
-							var coname = $("[name=coname]").val();
-							var number = $("[name=cgnumber]").val();
-							var supplier = $("[name=supplier]").val();
-							$("#rkList").datagrid('reload',{
-								model:escape(model),
-								coname:escape(coname),
-								supplier:escape(supplier),
-								number:number
-							});
-						}
-					}]
-				},
-				url:webRoot+'/purchasing/purchasing!listWaitRk.do',
-				cols:[{
-					label:'采购编号',
-					field:'number',
-					renderer:function(data,i){
-						
+			$("#form").buildform({
+				actions: {
+					"search": function() {
+						let params = this.getParamMap();
+						grid.load(params);
 					}
-				},{
-					label:'供应商名称',
-					field:'coname'
-				},{
-					label:'入库仓库',
-					field:'subck'
-				},{
-					label:'责任人',
-					field:'man'
-				},{
-					label:'货期',
-					field:'datetime'
-				}]
-			});
+				}
+			})
 		}
 	});
 	
 	tabs.addTab({
 		label:'入库单管理',
-		template:'template_list.mvc',
+		template:'in/template_list.mvc',
 		handler:function(){
 			
 			var form = $("#searchTable").buildform({
@@ -134,122 +87,99 @@ var warehousePath = webRoot+'/warehouse/warehouse.dispatch';
 				showNowDate:true
 			});
 			
-			$("#inListDatagrid").datagrid({
-				title:'入库单列表',
-				url:webRoot+"/warehouse/warehouse!listIn.do",
-				pageSize:20,
+			let grid;
+			let url = webRoot+"/warehouse/warehouse!listIn.do";
+			let target = $("#inListDatagrid");
+			grid = new BootstrapGrid({
+				url:url,
+				target:target,
 				cols:[{
 					label:'入库编号',
 					field:'number',
-					renderer:function(data){
+					renderer:function(value,data){
+						let href = webRoot+"/warehouse/in/view.mvc?id="+data.id;
 						var a = $("<a>",{
-							text:data.number,
-							click:function(){
-								window.open(webRoot+"/warehouse/in/view.mvc?id="+data.id);
-							}
+							text:value,
+							href:href,
+							target:'_blank'
 						});
-						return a;
-					}
-				},{
-					label:'供应商名称',
-					field:'supplier'
-				},{
-					label:'入库类别',
-					field:'int_types'
-				},{
-					label:'登记者',
-					field:'man'
-				},{
-					label:'入库日期',
-					field:'int_date'
-				},{
-					label:'当前状态',
-					field:'states'
-				}]
-			});
-		}
-	});
-	
-	tabs.addTab({
-		label:'待入库供应商退货订单',
-		template:'template_wait_refund.mvc',
-		handler:function(){
-			$("#refundList").datagrid({
-				title:'供应商退货列表: ',
-				url:webRoot+'/warehouse/warehouse!listRefund.do',
-				cols:[{
-					label:'退货编号',
-					field:'number',
-					renderer:function(data,i){
-						var purchasingDetailPath = webRoot+"/warehouse/in/supplier-refund-view.mvc?id="+data.id;
-						var a = $("<a>",{
-							href:purchasingDetailPath,
-							target:'_blank',
-							text:data.number
-						})
 						return a;
 					}
 				},{
 					label:'供应商名称',
 					field:'coname'
 				},{
-					label:'申请人',
+					label:'入库类别',
+					field:'int_types'
+				},{
+					label:'责任人',
+					field:'man'
+				},{
+					label:'入库日期',
+					field:'int_date'
+				}]
+			});
+			grid.init();
+		}
+	});
+	
+	tabs.addTab({
+		label:'待入库供应商退货订单',
+		template:'in/template_wait_refund.mvc',
+		handler:function(){
+			
+			let url = webRoot+'/warehouse/warehouse!listRefund.do';
+			let target = $("#refundList");
+			grid = new BootstrapGrid({
+				url:url,
+				target:target,
+				cols:[{
+					label:'退货编号',
+					field:'number',
+					renderer:function(value,data){
+						let href = webRoot+"/warehouse/in/supplier-refund-view.mvc?id="+data.id;
+						var a = $("<a>",{
+							text:value,
+							href:href,
+							target:'_blank'
+						});
+						return a;
+					}
+				},{
+					label:'供应商名称',
+					field:'coname'
+				},{
+					label:'责任人',
 					field:'man'
 				},{
 					label:'当前状态',
 					field:'state'
 				}]
 			});
-		}
-	});
-	
-	tabs.addTab({
-		label:'调货待入库',
-		template:'template_wait_dh.mvc',
-		handler:function(){
-			$("#dhList").datagrid({
-				title:'调货待入库: ',
-				url:warehousePath,
-				data:{
-					method:'listSampleDh'
-				},
-				cols:[{
-					label:'调货编号',
-					field:'number',
-					renderer:function(data,i){
-						var purchasingDetailPath = webRoot+"/sale/cypgl/ryp-view.jsp?id="+data.id;
-						return "<a href='"+purchasingDetailPath+"' target='_blank'>"+data.number+"</a>";
-					}
-				},{
-					label:'出库仓库',
-					field:'sub'
-				},{
-					label:'入库仓库',
-					field:'money'
-				},{
-					label:'申请人',
-					field:'man'
-				},{
-					label:'日期',
-					field:'datetime'
-				}]
-			});
+			grid.init();
 		}
 	});
 	
 	tabs.addTab({
 		label:'样品待入库',
-		template:'template_wait_sample.mvc',
+		template:'in/template_wait_sample.mvc',
 		handler:function(){
-			$("#sampleList").datagrid({
-				title:'样品待入库: ',
-				url:webRoot+'/warehouse/warehouse!listSample.do',
+			let url = webRoot+'/warehouse/warehouse!listSample.do';
+			let target = $("#sampleList");
+			grid = new BootstrapGrid({
+				url:url,
+				target:target,
 				cols:[{
 					label:'样品编号',
 					field:'number',
-					renderer:function(data,i){
-						var purchasingDetailPath = webRoot+"/sale/ypgl/ryp-view.jsp?id="+data.id;
-						return "<a href='"+purchasingDetailPath+"' target='_blank'>"+data.number+"</a>";
+					renderer:function(value,data){
+						let href = webRoot+"/sale/ypgl/ryp-view.jsp?id="+data.id;
+						var a = $("<a>",{
+							text:value,
+							href:href,
+							target:'_blank'
+						});
+						return a;
 					}
 				},{
 					label:'客户名称',
@@ -258,13 +188,14 @@ var warehousePath = webRoot+'/warehouse/warehouse.dispatch';
 					label:'运输方式',
 					field:'delivery_terms'
 				},{
-					label:'申请人',
+					label:'责任人',
 					field:'man'
 				},{
 					label:'登记日期',
 					field:'datetime'
 				}]
 			});
+			grid.init();
 		}
 	});
 	
